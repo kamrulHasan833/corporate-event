c9100f248e8e8cefb9623f4e20dd6563d4f966aa
Kamrul Hasan <kamrul3496hasan@gmail.com>
2023-10-09 22:39:33 +0600

    Some simple modification

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,6 @@ import Footer from "./components/shared/Footer";
 import Header from "./components/shared/Header";
 
 function App() {
-  let count = 0;
-  const nums = [0, 1, 2, 3];
-
-  nums.forEach((num) => {
-    if (num) count += 1;
-  });
-  console.log(count);
   useEffect(() => {
     AOS.init();
   }, []);
